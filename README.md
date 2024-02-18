9e4442f983de0a8b67bb07c18f115c28a6c49e9e
allexok <alexok997@gmail.com>
2024-02-18 21:57:32 +0100

    Add telegram bot example and examples description

--- README.md
+++ README.md
@@ -9,5 +9,26 @@ When compiling the examples ensure that:
 - The Board option in tools tab is set to 'ESP32C6 Dev Module'
 - USB CDC On Boot option in tools tab is set to 'True'
 - Port option in tools tab is set to the port, where you connected your board.
+## Quick examples description
+- esp32c6bugblink - The first example demonstrates the basic blinking functionality, nothing fancy, but a good way to start. 
+    - Parts needed:
+        - Esp32-C6-Bug
+- esp32c6bugethernet - The second example utizes the Ethernet to connect to the Internet, use it to ensure that Esp32-Bug-Eth is working.
+    - Parts needed:
+        - Esp32-C6-Bug
+        - Esp32-Bug-Eth
+- esp32c6bugplusSH1106_128x64_i2c_QTPY - For this example, you will need some kind of OLED display based around SH1106 driver. If it has Stemma QT connector you can connect it to Esp32-Bug-Eth via it(Alternatively just connect display SDA->pin 21, display SCL->pin 20). The code prints basic 'hello world' style message.  
+    - Parts needed:
+        - Esp32-C6-Bug
+        - Some Oled with SH1106 driver
+        - optional: Esp32-Bug-Eth
+- esp32c6bugethernetbmp280telegrambot - This example is more advanced and more 'open-ended', but is my personal favorite. It uses Ethernet to connect to the Internet, reads data from BMP280 sensor(you can later replace it with any other sensor/combination of sensors) and sends it to you via Telegram messenger if the data exceeds some limit(temperature range 20-30 °C). You can follow [this](https://randomnerdtutorials.com/telegram-control-esp32-esp8266-nodemcu-outputs/) tutorial to get Telegram token and Chat ID defines in code.
+I personally love this example, because I see it as a stepping stone to some interesting experiments. Imagine a group of sensors 'writing' data to a Telegram chat and an AI like ChatGPT reading the sensor data from the chat and commanding some other IoT devices to perform actions!(Like issuing 'open the window command' via the same chat) Can it handle the data like this? Will it open the window if the temperature is too hot? What is the maximum amount of 'inputs' and 'outputs' it can handle? Noone knows since the technology is quite new, maybe you will be the first person to find out?
+    - Parts needed:
+        - Esp32-C6-Bug
+        - BMP280 sensor(Something with Stemma QT works best)
+        - Esp32-Bug-Eth
+
+
 ## ESP-IDF examples
 If you are also interested in advanced features of Esp32-C6 like Zigbee and thread please refer to [Official examples](https://github.com/espressif/esp-idf/tree/release/v5.1/examples) and to Demos section of [Esp32-C6-Bug datasheet](https://github.com/allexoK/Esp32-C6-Bug-Docs/blob/main/esp32c6bugdatasheet.pdf) 
---
+++ examples/esp32c6bugethernetbmp280telegrambot/TelegramBot.ino
@@ -0,0 +1,121 @@
+/*
+  Karavaev Aleksei
+
+  Project created using Brian Lough's Universal Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+  Example based on the Universal Arduino Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot/blob/master/examples/ESP8266/FlashLED/FlashLED.ino
+
+*/
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>   // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <ArduinoJson.h>
+#include <SPI.h>
+#include <ETH.h>
+#include <Adafruit_BMP280.h>
+
+
+#define ETH_TYPE        ETH_PHY_W5500
+#define ETH_ADDR         1
+#define ETH_CS           5
+#define ETH_IRQ          4
+#define ETH_RST          -1
+
+// SPI pins
+#define ETH_SPI_SCK     6
+#define ETH_SPI_MISO    2
+#define ETH_SPI_MOSI    7
+
+
+#define LED_PIN         2
+#define ANALOG_IN       3
+#define TOP_THRESHHOLD    30
+#define BOTTOM_THRESHHOLD 20
+// Initialize Telegram BOT
+#define BOT_TOKEN "XXXXXXXXXXXXXXXXXXXX"  // your Bot Token (Get from Botfather)
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "XXXXXXX"
+static bool eth_connected = false;
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOT_TOKEN, client);
+
+// Checks for new messages every 1 second.
+int gasPostDelay = 1000;
+unsigned long lastTimeBotRan;
+Adafruit_BMP280 bmp; // I2C
+
+void onEvent(arduino_event_id_t event, arduino_event_info_t info)
+{
+  switch (event) {
+    case ARDUINO_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      //set eth hostname here
+      ETH.setHostname("esp32-eth0");
+      break;
+    case ARDUINO_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case ARDUINO_EVENT_ETH_GOT_IP:
+      ETH.printInfo(Serial);
+      eth_connected = true;
+      break;
+    case ARDUINO_EVENT_ETH_LOST_IP:
+      Serial.println("ETH Lost IP");
+      eth_connected = false;
+      break;
+    case ARDUINO_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      eth_connected = false;
+      break;
+    case ARDUINO_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      eth_connected = false;
+      break;
+    default:
+      break;
+  }
+}
+
+
+void readSensorDataAndPost(){
+    float sensorValue=bmp.readTemperature();
+    if(sensorValue>TOP_THRESHHOLD){
+      Serial.println("Sensor value is above top threshold:"+String(sensorValue));
+      bot.sendMessage(CHAT_ID, "Sensor value is above top threshold:"+String(sensorValue), "");
+    }
+    if(sensorValue<BOTTOM_THRESHHOLD){
+      Serial.println("Sensor value is below bottom threshold(sensor is probably disconnected):"+String(sensorValue));
+      bot.sendMessage(CHAT_ID, "Sensor value is below bottom threshold(sensor is probably disconnected):"+String(sensorValue), "");
+    }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("ESP32-C6-Bug Sensor Reader\n");
+  Wire.begin(21,20);
+  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    while (1) delay(10);
+  }
+  WiFi.onEvent(onEvent);
+  SPI.begin(ETH_SPI_SCK, ETH_SPI_MISO, ETH_SPI_MOSI);
+  ETH.begin(ETH_TYPE, ETH_ADDR, ETH_CS, ETH_IRQ, ETH_RST, SPI);
+  #ifdef ESP32
+    client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  #endif
+  
+}
+
+void loop() {
+  if(eth_connected){
+    if (millis() > lastTimeBotRan + gasPostDelay)  {
+      readSensorDataAndPost();
+      lastTimeBotRan = millis();
+    }
+  }
+  delay(1);
+}
